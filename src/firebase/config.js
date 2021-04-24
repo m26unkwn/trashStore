5dc7d8df2a27811551f5bc371bf625ad15fa2ec2
m26unkwn <shukla9915294376@gmail.com>
2021-04-24 23:28:29 +0530

    changed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import "firebase/storage";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBDqNrXUE7sD36n8993kf30-CijNBhsqrw",
-  authDomain: "photogallery-348d8.firebaseapp.com",
+  apiKey: "etataay hggngndgngn",
+  authDomain: "trjrjdrjdjdtjd",
   projectId: "photogallery-348d8",
-  storageBucket: "photogallery-348d8.appspot.com",
-  messagingSenderId: "820646503510",
-  appId: "1:820646503510:web:82fb967df3fadf81c487f7",
+  storageBucket: "pjdjtdrjdr",
+  messagingSenderId: "jdjtdrjrjrdtj",
+  appId: ":tjdrjdrrjdrj",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
